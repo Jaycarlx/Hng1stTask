3f45d4da00a7be2261ebae1f13e3e86a0ac58af2
Jaycarlx <molokwuchibuzorchristian@gmail.com>
2022-11-04 16:30:16 +0100

    Feat: Added and linked the contact page

--- hnglinktree/src/App.jsx
+++ hnglinktree/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Home from './pages/Home';
-import Contact from './pages/Contact'
+import Home from './Pages/Home';
+import Contact from './Pages/Contact'
 import { Routes, Route } from 'react-router-dom';
 // Comment
 
